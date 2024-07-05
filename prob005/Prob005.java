04fce08e2fa8138f369aeef2f450e300899aae22
taeyeon <garong0721@gmail.com>
2024-07-05 17:56:43 +0900

    new : 5번 대소문자 변환

---
+++ prob005/Prob005.java
@@ -0,0 +1,19 @@
+package prob005;
+
+import java.util.Scanner;
+
+public class Prob005 {
+    public static void main(String[] args) {
+        /* ty
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        for (char c : str.toCharArray()) {
+            System.out.print(Character.isUpperCase(c) ?
+                    Character.toLowerCase(c) : Character.toUpperCase(c));
+
+        }
+         */
+    }
+}
