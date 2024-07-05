3a021f252fcc9cee0809822a4d2b7e069b9d7ada
taeyeon <garong0721@gmail.com>
2024-07-05 17:49:44 +0900

    new : 4번 10진수를 2진수로 변환

---
+++ prob004/Prob004.java
@@ -0,0 +1,32 @@
+package prob004;
+
+import java.util.Scanner;
+
+public class Prob004 {
+    public static void main(String[] args) {
+        /* ty
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+//		방법 1.
+//		String bin = Integer.toBinaryString(n);
+//		System.out.println(bin);
+
+//		방법 2.
+        int bin[] = new int[100];
+        int i = 0;
+        int mok = n;
+        while(mok > 0) {
+            bin[i] = mok % 2;
+            mok /= 2;
+            i++;
+        }
+        i--;
+        for(; i >= 0; i--) {
+            System.out.print(bin[i]);
+        }
+         */
+    }
+
+
+}
