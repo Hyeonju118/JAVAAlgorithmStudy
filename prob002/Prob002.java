36875758fd404df785f761a9cfb389c79b2a1be1
Hyeonju <6841189@naver.com>
2024-07-08 08:56:49 +0900

    new : 2번 피보나치 수열

--- prob002/Prob002.java
+++ prob002/Prob002.java
@@ -37,5 +37,18 @@ public class Prob002 {
 			System.out.println(arr[i]);
 		}
          */
+        /*
+        int arr[] = new int[100];
+
+		arr[1] = 1;
+		arr[2] = 1;
+
+		for(int i = 3; i < 100; i++) {
+			arr[i] = arr[i-1] + arr[i-2];
+		}
+		for(int i = 1; i <= 10; i++) {
+			System.out.print(arr[i] + " ");
+		}
+         */
 	}
 }
