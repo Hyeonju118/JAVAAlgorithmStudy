3cd6e90651a35a3fb5bfa74dd617b07761b83677
JSW <tmddn1008@gmail.com>
2024-07-08 19:26:55 +0900

    new : 5번대소문자 변환

--- prob005/Prob005.java
+++ prob005/Prob005.java
@@ -2,6 +2,25 @@ package prob005;
 
 import java.util.Scanner;
 
+class JeongSeung05 {
+
+    void caseTrans(String str) {
+        StringBuilder sb = new StringBuilder();
+        char c = 0;
+        for(int i=0; i<str.length(); i++) {
+
+            if(str.charAt(i) < 92) {
+                c = (char) (str.charAt(i) + 32);
+            } else {
+                c = (char) (str.charAt(i) - 32);
+            }
+            sb.append(c);
+        }
+        System.out.println(sb);
+    }
+}
+
+
 public class Prob005 {
     public static void main(String[] args) {
         /* ty
@@ -15,5 +34,13 @@ public class Prob005 {
 
         }
          */
+    	
+    	//정승우
+    	/*
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        JeongSeung05 jeongSeung05 = new JeongSeung05();
+        jeongSeung05.caseTrans(str);
+    	*/
     }
 }
