648ab3914d62dd659dc2c0c0ef33e188933c2b62
Hyeonju <6841189@naver.com>
2024-07-08 17:34:59 +0900

    new : 4번 10진수를 2진수로 변환

--- prob004/Prob004.java
+++ prob004/Prob004.java
@@ -26,6 +26,24 @@ public class Prob004 {
             System.out.print(bin[i]);
         }
          */
+
+        /*
+         int inputNum = 19;
+        int bin[] = new int[100];
+
+        int i = 0;
+        int mod = inputNum;
+
+        while(mod > 0) {
+            bin[i] = mod % 2;
+            mod /= 2;
+            i++;
+        }
+        i--;
+        for(; i>=0; i--){
+            System.out.print(bin[i]);
+        }
+         */
     }
 
 
