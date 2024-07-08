253e65518b2ff0c162703e4a25436f561f1d4c3c
cookooyas <119028186+cookooyas@users.noreply.github.com>
2024-07-08 18:55:09 +0900

    Update Prob003.java

--- prob003/Prob003.java
+++ prob003/Prob003.java
@@ -22,6 +22,25 @@ class kiseok003 {
    }
 }
 
+class JeongSeung3 {
+
+    int findMode(int[] arr) {
+        int mode[] = new int[10];
+        for(int i=0; i<10; i++) {
+            mode[arr[i]]++;
+        }
+        int max = 0;
+        int idx = 0;
+        for(int i=0; i<10; i++) {
+            if(max<mode[i]) {
+                max = mode[i];
+                idx = i;
+            }
+        }
+        return idx;
+    }
+}
+
 public class Prob003 {
    public static void main(String[] args) {
 //         김태연
@@ -80,6 +99,15 @@ public class Prob003 {
       sc.nextLine();
       System.out.println(Arrays.toString(ans.mostVal(sc.nextLine())));
 
+       //정승우
+      int[] arr = new int[10];
+      for(int i=0; i<10; i++) {
+          arr[i] = sc.nextInt();
+      }
+      JeongSeung3 jeongSeung3 = new JeongSeung3();
+      System.out.println(jeongSeung3.findMode(arr));
+
+      
       sc.close();
    }
 
