8e9ee412e3de60e71e8f215cc663731486ddbd34
Hyeonju <6841189@naver.com>
2024-07-08 18:11:29 +0900

    new : 5번 대소문자 변환

--- prob005/Prob005.java
+++ prob005/Prob005.java
@@ -15,5 +15,21 @@ public class Prob005 {
 
         }
          */
+         /* hj
+          String input = "helloWorLD";
+        char []arr;
+        arr = input.toCharArray();
+
+        for (int i = 0; i<arr.length; i++ ){
+            if(arr[i] >= 'a' && arr[i] <= 'z'){
+                arr[i] = (char) (arr[i] + 'A' - 'a');
+            }
+            else if(arr[i] >= 'A' && arr[i] <='z'){
+                arr[i] =(char)(arr[i] -('A'-'a'));
+
+            }
+        }
+        System.out.print(arr);
+          */
     }
 }
